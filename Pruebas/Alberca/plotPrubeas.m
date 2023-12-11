6e50c399d298eba12d8c5afef88b0f32ec297921
Benjamin Nicolas <benjaminnt12@gmail.com>
2023-12-11 14:25:53 -0600

    pruebas control en alberca

---
+++ Pruebas/Alberca/plotPrubeas.m
@@ -0,0 +1,31 @@
+fid = fopen('control1.txt', 'r');  % Abre el archivo en modo lectura
+data = textscan(fid, 'yaw: %f yaw Deseada: %f Control: %f Error: %f');  % Lee los valores numéricos
+fclose(fid);  % Cierra el archivo
+
+% Extrae los datos
+yaw = data{1};
+yawDeseada = data{2};
+control = data{3};
+error = data{4};
+
+% Grafica los datos
+figure;
+
+subplot(2, 2, 1);
+plot(yaw);
+title('Yaw');
+
+subplot(2, 2, 2);
+plot(yaw, 'b'); hold on;
+plot(yawDeseada, 'r--');
+hold off;
+title('Yaw y Yaw Deseada');
+legend('Yaw', 'Yaw Deseada');
+
+subplot(2, 2, 3);
+plot(control);
+title('Control');
+
+subplot(2, 2, 4);
+plot(error);
+title('Error');
--- test/plotPrubeas.m
+++ test/plotPrubeas.m
@@ -1,31 +1,38 @@
-fid = fopen('control1.txt', 'r');  % Abre el archivo en modo lectura
-data = textscan(fid, 'yaw: %f yaw Deseada: %f Control: %f Error: %f');  % Lee los valores numéricos
-fclose(fid);  % Cierra el archivo
+% Obtiene una lista de todos los archivos en el directorio
+files = dir('~/Github/Matlab_algorithm/Pruebas/Alberca/Control/secuencia7/*.txt');
 
-% Extrae los datos
-yaw = data{1};
-yawDeseada = data{2};
-control = data{3};
-error = data{4};
+% Itera sobre cada archivo
+for file = files'
+    % Abre el archivo en modo lectura
+    fid = fopen(fullfile(file.folder, file.name), 'r');
+    data = textscan(fid, 'yaw: %f yaw Deseada: %f Control: %f Error: %f');  % Lee los valores numéricos
+    fclose(fid);  % Cierra el archivo
 
-% Grafica los datos
-figure;
+    % Extrae los datos
+    yaw = data{1};
+    yawDeseada = data{2};
+    control = data{3};
+    error = data{4};
 
-subplot(2, 2, 1);
-plot(yaw);
-title('Yaw');
+    % Grafica los datos
+    figure;
 
-subplot(2, 2, 2);
-plot(yaw, 'b'); hold on;
-plot(yawDeseada, 'r--');
-hold off;
-title('Yaw y Yaw Deseada');
-legend('Yaw', 'Yaw Deseada');
+    subplot(2, 2, 1);
+    plot(yaw);
+    title('Yaw');
 
-subplot(2, 2, 3);
-plot(control);
-title('Control');
+    subplot(2, 2, 2);
+    plot(yaw, 'b'); hold on;
+    plot(yawDeseada, 'r--');
+    hold off;
+    title('Yaw y Yaw Deseada');
+    legend('Yaw', 'Yaw Deseada');
 
-subplot(2, 2, 4);
-plot(error);
-title('Error');
+    subplot(2, 2, 3);
+    plot(control);
+    title('Control');
+
+    subplot(2, 2, 4);
+    plot(error);
+    title('Error');
+end
