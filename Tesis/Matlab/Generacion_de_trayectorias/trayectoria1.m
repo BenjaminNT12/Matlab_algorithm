294cf9b53c9122acfeb0fb6382a9c8fb619c8971
Benjamin Nicolas <benjaminnt12@gmail.com>
2024-05-17 23:38:30 -0600

    codigo para graficar las trayectorias

--- Tesis/Matlab/Generacion_de_trayectorias/trayectoria1.m
+++ Tesis/Matlab/Generacion_de_trayectorias/trayectoria1.m
@@ -4,9 +4,9 @@ close all
 T = 0:0.005:100;
 
 % Calcular yawDesired para cada valor de T
-yawDesired = rad2deg((sin(0.3*T)) + 0.5*cos(0.7*T));
+% yawDesired = rad2deg((sin(0.3*T)) + 0.5*cos(0.7*T));
 % yawDesired = rad2deg((sin(0.3*T)) + 0.5*cos(0.7*T)); t2
-% yawDesired = rad2deg((sin(0.5*T)) + 0.9*cos(0.4*T)); t1
+yawDesired = rad2deg((sin(0.5*T)) + 0.9*cos(0.4*T)); t1
 
 % Crear la gráfica
 figure;
