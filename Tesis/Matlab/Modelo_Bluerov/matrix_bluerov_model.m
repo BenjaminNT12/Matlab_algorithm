28c50f551b064b7355374b1513b30c29b97955fb
Benjamin Nicolas <benjaminnt12@gmail.com>
2024-06-07 15:32:46 -0600

    nuevas matrices para el modelo

--- Tesis/Matlab/Modelo_Bluerov/matrix_bluerov_model.m
+++ Tesis/Matlab/Modelo_Bluerov/matrix_bluerov_model.m
@@ -2,7 +2,8 @@ clear
 close all
 syms x y z phi theta psi u v w p q r T1 T2 T3 T4
 syms I_x I_y I_z I_xy I_xz I_yz I_yx I_zx I_zy
-syms x_g y_g z_g
+syms x_g y_g z_g 
+syms m u_p r_p q_p
 
 eta = [x y z phi theta psi]';
 nu = [u v w p q r]';
@@ -29,15 +30,32 @@ I_3x3 = eye(3);
 
 r_bg = [x_g y_g z_g]';
 
-S = Smtrx(r_bg)
+S = Smtrx(r_bg);
+
+M_rb = [m*I_3x3 -m*S;
+        m*S      I_o];
+
+
+X = m*(u_p - v*r + w*q - x_g*(q^2 + r^2) + y_g*(p*q - r_p) + z_g*(p*r + q_p));
+Y = m*(v_p - w*p + u*r - y_g*(r^2 + p^2) + z_g*(q*r - p_p) + x_g*(q*p + r_p));
+Z = m*(w_p - u*q + v*p - z_g*(p^2 + q^2) + x_g*(r*p - q_p) + y_g*(r*q + p_p));
+K = I_x*p_p + (I_z - I_y)*q*r - (r_p + p*q)*I_xz + (r^2 - q^2)*I_yz + (p*r - q_p)*I_xy + m*(y_g*(w_p - u*q + v*p) - z_g*(v_p - w*p + u*r));
+M = I_y*q_p + (I_x - I_z)*r*p - (p_p + q*r)*I_xy + (p^2 - r^2)*I_zx + (q*p - r_p)*I_yz + m*(z_g*(u_p - v*r + w*q) - x_g*(w_p - u*q + v*p));
+N = I_z*r_p + (I_y - I_x)*p*q - (q_p + r*p)*I_yz + (q^2 - p^2)*I_xy + (r*q - p_p)*I_zx + m*(x_g*(v_p - w*p + u*r) - y_g*(u_p - v*r + w*q));
+
+
+
+
+
+
+
+
+
+
+
+
 
 function S=Smtrx(r)
-    % S=SMTRX(r) computes the 3x3 vector cross product matrix S=-S'
-    % such that rxt=S(r)t is true for all 3x1 vectors r and t
-    %
-    % Author:   Thor I. Fossen
-    % Date:     14th June 2001
-    % Revisions:
     S=[0,-r(3),r(2);...
         r(3),0,-r(1);...
         -r(2),r(1),0];
